e7d5ec2650d3bbad648115447b4d339a53c7286c
yorrdt <this.mazahaka@gmail.com>
2022-07-10 15:42:21 +0300

    link libs glew and glm, new README

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
 # opengl-practice
 
-Code::Blocks IDE
+Improving practical skills in OpenGL
+
+## IDE
+
+Code::Blocks with MinGW
 
 ## Libraries
 
 - GLFW
+- GLEW
+- GLM
 
+**Building libraries (except GLM, because GLM is header only library) through CMake.**
+
+## Linking to IDE
+
+### Build options... -> Linker settings -> Link libraries:
+
+- glew32
+- glu32
+- opengl32
+- glfw3
+- gdi32
+
+**Must be in that order!**
+
+![Illustration](https://github.com/yorrdt/opengl-practice/blob/main/illustrations/linker_settings-link_libraries.jpg)
+
+### Build options... -> Search directories -> Compiler:
+
+- "sources\include",
+- "sources\include\GLEW",
+- "sources\include\GLFW"
+
+**Warning: relative path.**
+
+![Illustration](https://github.com/yorrdt/opengl-practice/blob/main/illustrations/search_directories-compiler.jpg)
+
+### Build options... -> Search directories -> Linker:
+
+- "sources\lib",
+- "sources\lib\GLEW",
+- "sources\lib\GLFW"
+
+**Warning: relative path.**
+
+![Illustration](https://github.com/yorrdt/opengl-practice/blob/main/illustrations/search_directories-linker.jpg)
+
+### Build options... -> Compiler settings -> #defines:
+
+- GLEW_STATIC
+
+![Illustration](https://github.com/yorrdt/opengl-practice/blob/main/illustrations/compiler_settings-defines.jpg)
+
+## Build and run
+
+![Illustration](https://github.com/yorrdt/opengl-practice/blob/main/illustrations/build_and_run.jpg)
---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* glfwWindow = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window", NULL, NULL);
+
+    if (glfwWindow == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(glfwWindow);
+    glfwSetFramebufferSizeCallback(glfwWindow, framebuffer_size_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(glfwWindow))
+    {
+        processInput(glfwWindow);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(glfwWindow);
+        glfwPollEvents();
+    }
+
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
